2b8bd42587b29fc13941935ff7f94d95b25fc683
kamandkrg <kamandkargar@gmail.com>
2022-07-16 00:04:16 +0430

    add model in user, product and category and add category app to project

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
-    'product.apps.ProductConfig'
+    'product.apps.ProductConfig',
+    'category.apps.CategoryConfig'
 ]
 
 MIDDLEWARE = [
--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from category.models import Category
 
-# Create your models here.
+
+class Product(models.Model):
+    type = models.CharField(max_length=32)
+    name = models.CharField(max_length=64)
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.PROTECT)
+    amount = models.IntegerField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    description = models.TextField()
+
