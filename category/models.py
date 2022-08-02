81cab40819f110b717de60578d17924816c485da
kamandkrg <kamandkargar@gmail.com>
2022-08-02 11:48:33 +0430

    add class image to calegory

--- category/models.py
+++ category/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=32)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    slug = models.SlugField()
+
+
+class CategoryImage(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='categories/')
 
 
 
