6c44323536aa4e265e51093f6a13d0cc9b01210b
kamandkrg <kamandkargar@gmail.com>
2022-09-02 13:39:31 +0430

    fix pagination to list

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -175,5 +175,6 @@ REST_FRAMEWORK = {
     ) if DEBUG else (
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
-    'DEFAULT_PAGINATION_CLASSES': 'rest_framework.pagination.PageNumberPagination'
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
